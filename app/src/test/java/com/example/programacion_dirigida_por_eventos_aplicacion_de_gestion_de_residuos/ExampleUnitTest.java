ba604eddf8ad69e7bfafb065b1cc4decd9cf02a9
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-09-24 00:25:03 +0200

    Initial commit

---
+++ app/src/test/java/com/example/programacion_dirigida_por_eventos_aplicacion_de_gestion_de_residuos/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.programacion_dirigida_por_eventos_aplicacion_de_gestion_de_residuos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Programacion_Dirigida_Por_Eventos_Aplicacion_de_Gestion_de_Residuos"
+include(":app")
+ 
